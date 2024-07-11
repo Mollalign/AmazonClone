3b3d6e3aecfb72b3c0885e85894dceccb556fc8a
mollalign <molledan69@gmail.com>
2024-07-11 05:33:59 -0700

    Remove default tests and add money test

--- test/moneyTest.js
+++ test/moneyTest.js
@@ -3,15 +3,27 @@ import { formatCurrency } from "../scripts/utils/money.js";
 
 console.log('test suits: formatCurrency');
 
+console.log('convert cents into dollalrs');
+
 if(formatCurrency(2095) === '20.95') {
   console.log('passed');
 } else {
   console.log('failed');
 }
 
+console.log('');
 
-if (formatCurrency(2000.5) === '20.01') {
+if (formatCurrency(0) === '0.00') {
  console.log('passed');
 } else {
   console.log('failed');
 }
+
+console.log('');
+
+if (formatCurrency(2000.5) === '20.01') {
+  console.log('passed');
+ } else {
+   console.log('failed');
+ }
+ 
