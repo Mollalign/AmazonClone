a87acd7e4c74abeef6121d4f8e7165e84bea3d42
mollalign <molledan69@gmail.com>
2024-07-19 01:36:10 -0700

    Create oop version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,7 @@
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import { renderOrderSummery } from "./checkout/orderSummery.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
+import '../data/cart-oop.js'
 
 renderCheckoutHeader();
 
