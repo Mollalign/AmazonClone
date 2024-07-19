a9fde399cb59923e54385f461d93cdbd7e64a8f5
mollalign <molledan69@gmail.com>
2024-07-19 03:13:53 -0700

    create class version of cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderCheckoutHeader} from './checkout/checkoutHeader.js';
 import { renderOrderSummery } from "./checkout/orderSummery.js";
 import { renderPaymentSummery } from "./checkout/paymentSummery.js";
-import '../data/cart-oop.js'
+import '../data/cart-class.js'
 
 renderCheckoutHeader();
 
